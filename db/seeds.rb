dd282167cfd613d8179ab6b1b902ae6d80350f2a
jasonodoom <jasonodoom@riseup.net>
2018-11-06 16:51:37 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Coupon.create(coupon_code: "58493030303", store: "Walmart")
+Coupon.create(coupon_code: "84037520483", store: "Tesco")
+Coupon.create(coupon_code: "12345678901", store: "ShopRite")
+Coupon.create(coupon_code: "74930857273", store: "Krispy Kremes")
+Coupon.create(coupon_code: "34953948598", store: "Target")
+Coupon.create(coupon_code: "65382889279", store: "Nordstrom")
